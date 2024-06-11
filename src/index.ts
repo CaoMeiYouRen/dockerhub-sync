a187275735a007a1d16ea49e95d105590a1ac873
CaoMeiYouRen <996881204@qq.com>
2024-06-11 23:02:58 +0800

    chore: 同步一个月内的镜像

--- src/index.ts
+++ src/index.ts
@@ -36,9 +36,9 @@ if (!destinationCredentials?.length) {
 
 const sourceTransport = 'docker'
 const destinationTransport = 'docker'
-const limit = 10
+const limit = 5
 const syncFormat = 'v2s2'
-const filterTime = 48 * 60 * 60 // 48 hours in seconds 172800
+const filterTime = 30 * 24 * 60 * 60 // 48 hours in seconds 172800
 
 for (const sourceRepo of sourceRepos) {
     console.log(`Syncing ${sourceRepo} to multiple destinations`)
